ce0d0fb50abe89484bd88a79f47fdaa78c4b209d
Rob McVey <rob.mcvey@announcemedia.com>
2011-09-17 14:55:48 -0500

    First commit

---
+++ answers_example.php
@@ -0,0 +1,33 @@
+<?php
+require_once 'answers_api.php';
+
+// set api key
+answers_api::$key = 'AMSUMMIT';
+// likely replace by $_SERVER['REMOTE_ADDR']
+answers_api::$ip  = '67.196.156.93';
+// turn on curl debuggin by switching this to true
+answers_api::$debug = false;
+
+$ask_response = answers_api::ask("What's up with that?");
+
+print_r($ask_response);
+// standard search example using default options
+$search_response = answers_api::search('When does the bacon narwhal?');
+
+// paged and filtered search example
+$page = 2;
+$limit = 5;
+$filters = array(
+	'filters' => 'answered',
+	'corpus' => 'wiki'
+);
+$paged_response = answers_api::search('Why is the sky blue?', $filters, $page, $limit);
+
+// categorize example
+$cat_response = answers_api::categorize('The bacon narwhals at midnight');
+
+print_r($search_response);
+print_r($paged_response);
+print_r($cat_response);
+
+?>
