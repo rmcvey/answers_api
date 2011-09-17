1f8b734719758707bf4e25bf2e1763956185b4a0
Rob McVey <rob.mcvey@announcemedia.com>
2011-09-17 15:15:31 -0500

    Updated example code

--- answers_example.php
+++ answers_example.php
@@ -1,18 +1,20 @@
 <?php
 require_once 'answers_api.php';
 
-// set api key
+// set api key and client IP address
 answers_api::$key = 'YOUR_API_KEY';
 answers_api::$ip  = $_SERVER['REMOTE_ADDR'];
-
-// turn on curl debuggin by switching this to true
+// turn on curl debugging by switching this to true
 answers_api::$debug = false;
 
 $ask_response = answers_api::ask("What's up with that?");
 
 print_r($ask_response);
+
 // standard search example using default options
 $search_response = answers_api::search('When does the bacon narwhal?');
+
+print_r($search_response);
 
 // paged and filtered search example
 $page = 2;
@@ -23,11 +25,12 @@ $filters = array(
 );
 $paged_response = answers_api::search('Why is the sky blue?', $filters, $page, $limit);
 
+print_r($paged_response);
+
+
 // categorize example
 $cat_response = answers_api::categorize('The bacon narwhals at midnight');
 
-print_r($search_response);
-print_r($paged_response);
 print_r($cat_response);
 
 ?>
